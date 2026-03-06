0db5543f551ebc67d38eccd4bb3827c145123d6a
dfhhgh <amirtamo3@gmail.com>
2026-03-06 06:53:31 +0200

    ininitialize project

--- lib/features/PageView/widgets/Pages.dart
+++ lib/features/PageView/widgets/Pages.dart
@@ -5,17 +5,17 @@ class PagesView extends StatelessWidget {
   PagesView({super.key});
   List<Pagewithimageandtext> pages = [
     Pagewithimageandtext(
-      Imagurl: 'assets/images/Balloons-cuate 1.png',
+      Imagurl: 'assets/images/Balloons-cuate.png',
       txt:
           'تطبيق طريقي منصة مصرية ذكية لتعزيز أمن وسلامة الطرق، تهدف إلى رصد وتطوير البنية التحتية باستخدام تقنيات متطورة لضمان تنقل آمن ومستدام.',
     ),
     Pagewithimageandtext(
-      Imagurl: 'assets/images/Business merger-pana 1.png',
+      Imagurl: 'assets/images/Businessmerger-pana1.png',
       txt:
           'تطبيق طريقي منصة مصرية ذكية لتعزيز أمن وسلامة الطرق، تهدف إلى رصد وتطوير البنية التحتية باستخدام تقنيات متطورة لضمان تنقل آمن ومستدام.',
     ),
     Pagewithimageandtext(
-      Imagurl: 'assets/images/Navigation-pana 1.png',
+      Imagurl: 'assets/images/Navigation-pana1.png',
       txt:
           'تطبيق طريقي منصة مصرية ذكية لتعزيز أمن وسلامة الطرق، تهدف إلى رصد وتطوير البنية التحتية باستخدام تقنيات متطورة لضمان تنقل آمن ومستدام.',
     ),
@@ -27,15 +27,32 @@ class PagesView extends StatelessWidget {
         builder: (context, constraints) {
           double w = constraints.maxWidth;
           double h = constraints.maxHeight;
-          return Column(
-            children: [
-              PageView.builder(
-                itemBuilder: (context, index) {
-                  return pages[index];
-                },
-                itemCount: pages.length,
+          return SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+
+                    children: [
+                      SizedBox(
+                        width: w * 0.6,
+                        height: w * 0.3,
+
+                        child: PageView.builder(
+                          itemBuilder: (context, index) {
+                            return pages[index];
+                          },
+                          itemCount: pages.length,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ),
           );
         },
       ),
